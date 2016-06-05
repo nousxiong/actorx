6e57c76192bbc803030dd918362b6335161a6d21
nousxiong <348944179@qq.com>
2016-06-05 10:53:11 +0800

    add actorx all.hpp

---
+++ include/actorx/all.hpp
@@ -0,0 +1,11 @@
+///
+/// all.hpp
+///
+
+#pragma once
+
+#include "actorx/asev/all.hpp"
+#include "actorx/cque/all.hpp"
+#include "actorx/coctx/all.hpp"
+#include "actorx/csegv/all.hpp"
+#include "actorx/utest/utest.hpp"

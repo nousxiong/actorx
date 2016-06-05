d4d323791898fa22a41b181bdf955cc19263be8e
nousxiong <348944179@qq.com>
2016-06-05 10:53:35 +0800

    add cmake

---
+++ libs/src/src.cpp
@@ -0,0 +1,10 @@
+///
+/// Copyright (c) 2016 Nous Xiong (348944179 at qq dot com)
+///
+/// Distributed under the Boost Software License, Version 1.0. (See accompanying
+/// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+///
+/// See https://github.com/nousxiong/actorx for latest version.
+///
+
+#include <actorx/all.hpp>
